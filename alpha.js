8fa1e7680a84c98a4f99e3124c96bf1bfdde11f4
Soyeb-Rupak <soyeb.wd@gmail.com>
2024-02-19 21:54:17 +0600

    score section update

--- alpha.js
+++ alpha.js
@@ -7,16 +7,73 @@
 
 
 // }
+function handleKeyboardButtonPress( event){
+    const playerPressed= event.key
+    console.log('player Pressed', playerPressed);
 
+    const currentAlphabetElement=document.getElementById('currentAlphabet');
+    const currentAlphabet=currentAlphabetElement.innerText;
+    const expectedAlphabet=currentAlphabet.toLowerCase()
+   console.log(playerPressed, expectedAlphabet );
+  
+    
+// cheeked match or not
+if(playerPressed===expectedAlphabet){
+
+const currentScore=getTextElementValueById('current-score');
+const updateScore=currentScore+1;
+setTextElementValueById('current-score', updateScore);
+
+
+// const currentScoreElement=document.getElementById('current-score')
+// const currentScoreText=currentScoreElement.innerText;
+// const currentScore=parseInt(currentScoreText);
+// console.log(currentScore);
+// const newScore=currentScore+1;
+// currentScoreElement.innerText=newScore;
+
+    removeBackgroundColorById(expectedAlphabet)
+   continueGame();
+}else{
+    console.log('you loser');
+
+    const currentLife= getTextElementValueById('current-life');
+    const updateLife=currentLife-1;
+    setTextElementValueById('current-life', updateLife);
+
+    // const currentLife=document.getElementById('current-life')
+    // const currentLifeText=currentLife.innerText;
+    // const presentLife=currentLifeText-1
+    // console.log(presentLife);
+    // currentLife.innerText=presentLife;
+}
+
+
+
+
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress )
 
 function continueGame(){
 
     const alphabet =getRandomAlphabet();
-    console.log(alphabet);
+    // console.log('your Random Alphabet', alphabet);
+  
+    // const alphabetUppercase=alphabet.toUpperCase()
+
+    const currentAlphabetElement=document.getElementById('currentAlphabet');
+    currentAlphabetElement.innerText=alphabet;
+    
+
+   
+     // set Back Ground Color-----------
+     setBackgroundColorById(alphabet)
     }
 
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
     continueGame();
+    
 }
--- utility/utility.js
+++ utility/utility.js
@@ -9,17 +9,42 @@ function showElementById(elementId){
 
 }
 
+// kbd show-----------
+function setBackgroundColorById(elementId){
+    const element=document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+
+   
+}
+
+function removeBackgroundColorById(elementId){
+    const element=document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+function getTextElementValueById(elementId){
+    const element=document.getElementById(elementId);
+    const elementTextValue=element.innerText;
+    const value=parseInt(elementTextValue);
+    return value;
+}
+function setTextElementValueById(elementId, value){
+const element = document.getElementById(elementId);
+element.innerText= value;
+
+}
+
 function getRandomAlphabet(){
     const alphabetString= 'abcdefghijklmnopqrstuvwxyz';
-    const alphabets = alphabetString.split(' ');
-    // console.log(alphabets);
+    const alphabets = alphabetString.split('');
+    console.log('random alphbet', alphabets);
 
     const randomNumber= Math.random()*25
     const index = Math.round(randomNumber);
-    
+    console.log(index);
 
     const alphabet=alphabets[index];
-    console.log(index, alphabet );
+    // console.log(index, alphabet );
     return alphabet;
 
 }
